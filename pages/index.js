9601b020ed4f05227c4a0aef4c54e3e228cb19a6
Saywa94 <saywachavez@gmail.com>
2022-06-26 12:19:47 -0400

    pages, links and more

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,21 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="container">
+      
       <Head>
-        <title>Create Next App</title>
+        <title>Learn Next App!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read{' '}
+          <Link href='/posts/first-post'>
+            <a>This page!</a>
+          </Link>
         </h1>
 
         <p className="description">
